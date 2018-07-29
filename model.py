2e81d2fe653deae0eac9a7083a78a1157ca48582
ioneaxelrod <ioneaxelrod@gmail.com>
2018-07-29 01:00:24 +0000

    database is working

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 """Models and database functions for Connector project."""
 
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from jinja2 import StrictUndefined
+from os import environ
+
 
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
@@ -23,6 +27,8 @@ class Volunteer(db.Model):
     password = db.Column(db.String(64), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
 
+    # organizations = db.relationship("OrganizationVolunteer", backref=db.backref("organization_volunteers",
+    #                                                                             order_by=volunteer_id))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
@@ -30,7 +36,16 @@ class Volunteer(db.Model):
         return "<Volunteer name={}, phone={}>".format(self.name, self.phone_number)
 
 
+    def retrieve_organizations_volunteering_for(self):
+        organization_ids = OrganizationVolunteer.query.filter_by(volunteer_id=self.volunteer_id)
 
+
+#     user = db.relationship("User",
+#                            backref=db.backref("ratings", order_by=rating_id))
+#
+#     movie = db.relationship("Movie",
+#                             backref=db.backref("ratings", order_by=rating_id))
+#
 
 ##############################################################################
 # Organization definition
@@ -42,18 +57,22 @@ class Organization(db.Model):
 
     organization_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
     address = db.Column(db.String(64), nullable=False)
-    category_code = db.Column(db.Integer, nullable=False)
+    category_code = db.Column(db.Integer, db.ForeignKey('categories.category_code'),nullable=False)
     description = db.Column(db.String(256))
     website = db.Column(db.String(64))
     # TODO Implement hours in a non stupid way
-
+    #
+    # volunteers = db.relationship("OrganizationVolunteer", backref=db.backref("organization_volunteers",
+    #                                                                             order_by=organization_id))
+    category = db.relationship("Category", backref=db.backref("categories", order_by=category_code))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
         return "<Organization name={}, category_code={}>".format(self.name, self.category_code)
-
 
 
 ##############################################################################
@@ -66,7 +85,6 @@ class Category(db.Model):
 
     category_code = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
-
 
     def __repr__(self):
         """Provide helpful representation when printed."""
@@ -82,9 +100,12 @@ class OrganizationVolunteer(db.Model):
 
     __tablename__ = "organization_volunteers"
 
-    volunteer_id = db.Column(db.Integer, nullable=False)
-    organization_id = db.Column(db.Integer, nullable=False)
+    orgvol_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    volunteer_id = db.Column(db.Integer, db.ForeignKey('volunteers.volunteer_id'), nullable=False)
+    organization_id = db.Column(db.Integer, db.ForeignKey('organizations.organization_id'), nullable=False)
 
+    organizations = db.relationship("Organization", backref=db.backref("organizations", order_by=organization_id))
+    volunteers = db.relationship("Volunteer", backref=db.backref("volunteers", order_by=volunteer_id))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
@@ -142,6 +163,62 @@ class OrganizationVolunteer(db.Model):
 ##############################################################################
 # Helper functions
 
+def create_dummy_volunteers():
+    """Create dummy volunteers for testing purposes"""
+
+    kami = Volunteer(name="Kami",
+                     email="kami@kami.com",
+                     password=environ['kami_pass'],
+                     phone_number=environ['kami_num'])
+    jennifer = Volunteer(name="Jennifer",
+                         email="jen@jennifer.com",
+                         password=environ['jen_pass'],
+                         phone_number=environ['jen_num'])
+    ione = Volunteer(name="Ione",
+                         email="ione@ione.com",
+                         password=environ['ione_pass'],
+                         phone_number=environ['ione_num'])
+
+    db.session.add_all([kami, jennifer, ione])
+    db.session.commit()
+    return [kami, jennifer, ione]
+
+
+def create_dummy_category():
+
+    food_aid = Category(name="food aid")
+    db.session.add(food_aid)
+    return food_aid
+
+
+
+def create_dummy_organization():
+    """"""
+    hackoak = Organization(name="HackOak",
+                           email="hackoak@hackoak.com",
+                           password=environ['oak_pass'],
+                           address="221 Main St, San Francisco, CA",
+                           category_code=1,
+                           description="Dedicated to helping out our peeps",
+                           )
+
+    db.session.add(hackoak)
+    db.session.commit()
+
+    return hackoak
+
+def create_dummy_orgvol(volunteers, organization):
+    organization_volunteers = []
+    for volunteer in volunteers:
+        organization_volunteer = OrganizationVolunteer(volunteer_id=volunteer.volunteer_id,
+                                                       organization_id=organization.organization_id)
+        organization_volunteers.append(organization_volunteer)
+
+    db.session.add_all(organization_volunteers)
+    db.session.commit()
+
+    return organization_volunteers
+
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
@@ -157,7 +234,27 @@ if __name__ == "__main__":
     # As a convenience, if we run this module interactively, it will leave
     # you in a state of being able to work with the database directly.
 
-    from server import app
+    app = Flask(__name__)
+
+    # Required to use Flask sessions and the debug toolbar
+    app.secret_key = "ABC"
+
+    # Normally, if you use an undefined variable in Jinja2, it fails
+    # silently. This is horrible. Fix this so that, instead, it raises an
+    # error.
+    app.jinja_env.undefined = StrictUndefined
+
+    # Connect database to flask
     connect_to_db(app)
+
+    # Drop old tables before adding dummy info
+    db.drop_all()
+    db.create_all()
+    dummies = create_dummy_volunteers()
+    category = create_dummy_category()
+    organization = create_dummy_organization()
+    volunteerList = create_dummy_orgvol(dummies, organization)
+
+
     print("Connected to DB.")
 
